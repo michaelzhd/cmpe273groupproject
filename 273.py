cbc440296983beb737237d961a5653337c1e485c
57322741 <57322741@qq.com>
2015-10-21 15:33:43 -0700

    keys deleted

--- 273.py
+++ 273.py
@@ -1,8 +1,8 @@
-import boto3,datetime
+import boto3,datetime,time
 from boto3.session import Session
 
-session = Session(aws_access_key_id='AKIAJBEOVR653DALXMAQ',
-				aws_secret_access_key='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',
+session = Session(aws_access_key_id='',#'',
+				aws_secret_access_key='',#'
 				region_name='us-west-1')
 
 ec2 = session.resource('ec2')
@@ -12,22 +12,29 @@ instances = ec2.instances.filter(
 
 client=session.client('cloudwatch')
 
-response = client.get_metric_statistics(
-				Namespace='AWS/EC2',
-				MetricName='CPUUtilization',
-				Dimensions=[
-						{
-								'Name':'InstanceId',
-								'Value':'i-19b37dab'
-						},
-				],
-				StartTime=datetime.datetime.now(),
-				EndTime=datetime.datetime.now() + datetime.timedelta(seconds=120),
-				Period=60,
-				Statistics=[
-						'Average'
-				],
-				Unit='Percent'
-			)
-cpuUsage = response['Datapoints'][0]['Average']
+for i in range(100):
+    response = client.get_metric_statistics(
+    				Namespace='AWS/EC2',
+    				MetricName='NetworkIn',
+    				Dimensions=[
+    						{
+    								'Name':'InstanceId',
+    								'Value':'i-d8400d1e'#'i-19b37dab'
+    						},
+    				],
+    				StartTime=datetime.datetime.now()- datetime.timedelta(seconds=120),
+    				EndTime=datetime.datetime.now(),
+    				Period=60,
+    				Statistics=[
+    						'Average'
+    				],
+    				Unit='Bytes'
+    			)
+    for key in response:
+        print key, ':', response[key]
+    # cpuUsage = response['Datapoints'][0]['Average']
+    # print cpuUsage
+    time.sleep(1)
+
+
 
